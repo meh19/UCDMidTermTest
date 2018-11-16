bc8f6a6d5cff8ab2604efa9dfe696e1ad7fb7d21
meh19@students.ptcollege.edu <>
2018-11-16 13:29:53 -0500

    Le code.

---
+++ src/ucdmidterm/UCDMidTerm.java
@@ -0,0 +1,15 @@
+/*
+ * Madison Hoffman
+ * User-Centered Design and Testing
+ * Mid Term
+ */
+package ucdmidterm;
+
+public class UCDMidTerm 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hi, my name is Madi and yes, my initials are 'MEH'.");
+    }
+    
+}
